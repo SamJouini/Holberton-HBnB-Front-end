66917d3bc5cfeb4983625a881ef8af18d10c07d2
SamJouini <samantha.u.h.jouini@gmail.com>
2024-08-08 12:51:05 +0200

    Project reorganization
    mostly based on https://flask.palletsprojects.com/en/3.0.x/tutorial

---
+++ flask/auth.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, abort
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route("/login", methods=['POST'])
+def login():
+    email = request.json['email']
+    password = request.json['password']
+    user_id = authenticate(email, password)
+
+    if user_id is not None:
+        payload =  {"user_id": str(user_id)}
+        token = jwt.encode(payload, app.config["SECRET_KEY"])
+        return {"access_token": token}
+
+
+    abort(401, "Utilisateur inconnu")
+    """authentification de l'utilisateur = vérifier login + mdp existe
+    construire la clé"""
+
+def authenticate(email, password):
+    if email == "sam@jouini.fr":
+        return 1
+    return None
--- flask/hbnb.py
+++ flask/hbnb.py
@@ -9,27 +9,11 @@ app.config.from_mapping(
     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
 )
 
-@app.route("/login", methods=['POST'])
-def login():
-    email = request.json['email']
-    password = request.json['password']
-    user_id = authenticate(email, password)
-
-    if user_id is not None:
-        payload =  {"user_id": str(user_id)}
-        token = jwt.encode(payload, app.config["SECRET_KEY"])
-        return {"access_token": token}
+from auth import auth
+app.register_blueprint(auth)
 
 
-    abort(401, "Utilisateur inconnu")
-    """authentification de l'utilisateur = vérifier login + mdp existe
-    construire la clé"""
-
-def authenticate(email, password):
-    if email == "sam@jouini.fr":
-        return 1
-    return None
-
-@app.route('/login.html')
-def login_page():
-    return app.send_static_file('login.html')
+@app.route('/', defaults={'page': 'index'})
+@app.route('/<page>.html')
+def show(page):
+    return app.send_static_file(f'{page}.html')
